a364294cc8962042e76c059aaf9e5547bcfc230f
Cunha Fonte <Cunha Fonte>
2016-06-29 10:43:05 +0100

    End work

---
+++ web_app/app/scripts/channel.js
@@ -0,0 +1,88 @@
+'use strict';
+
+$(document).ready(function () {
+    $('body').addClass(navigator.appVersion + ' is-js');
+
+    controlNavbar();
+    checkTransparent();
+    channel();
+
+    $(window).resize(function () {
+        controlNavbar();
+        checkTransparent();
+    });
+
+    //TOPBAR
+    $('#topbar').scrollupbar();
+
+    function controlNavbar() {
+        if ($(window).width() < 992) {
+            $('#topbar').addClass('navbar-white');
+            $('#topbar').removeClass('navbar-transparent');
+            $(".brand-img").attr("src", "images/logo.svg");
+        } else {
+            $('#topbar').removeClass('navbar-white');
+            $('#topbar').addClass('navbar-transparent');
+            $(".brand-img").attr("src", "images/logo-w.svg");
+        }
+    }
+});
+
+function checkTransparent() {
+
+    var transparent = true;
+    var hasTransparent = false;
+
+    $(document).ready(function () {
+        if ($('nav[role="navigation"]').hasClass('navbar-transparent')) {
+            hasTransparent = true;
+        }
+    });
+
+    $(document).scroll(function () {
+        if (hasTransparent) {
+            if ($(window).width() > 992) {
+                if ($(this).scrollTop() > 280) {
+                    if (transparent) {
+                        transparent = false;
+                        $('nav[role="navigation"]').removeClass('navbar-transparent');
+                        $('nav[role="navigation"]').addClass('navbar-white');
+                        $(".brand-img").attr("src", "images/logo.svg")
+                    }
+                } else {
+                    if (!transparent) {
+                        transparent = true;
+                        $('nav[role="navigation"]').removeClass('navbar-white');
+                        $('nav[role="navigation"]').addClass('navbar-transparent');
+                        $(".brand-img").attr("src", "images/logo-w.svg")
+                    }
+                }
+            }
+        }
+    });
+}
+
+function channel() {
+    var viewer = UstreamEmbed('UstreamIframe');
+
+    viewer.addListener('live', onEmbedEvent);
+    viewer.addListener('offline', onEmbedEvent);
+    viewer.addListener('playing', onEmbedEvent);
+    viewer.addListener('finished', onEmbedEvent);
+}
+
+var onEmbedEvent = function (event, data) {
+
+    console.log(event);
+
+    switch (event) {
+        case "live":
+            break;
+        case "offline":
+            break;
+        case "playing":
+            break;
+        case "finished":
+            break;
+    }
+}
